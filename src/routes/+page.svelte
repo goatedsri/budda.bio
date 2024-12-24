ad3da382bee338dd2a99896bb68d8ccb91a1dcba
dontdiecoding <spbuddave@gmail.com>
2024-12-24 03:19:14 -0600

    small fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,12 +43,7 @@
                     </div>
                 </a>
                 <div class="flex items-center gap-5 mt-6 md:mt-0 xl:gap-6 md:ml-auto">
-                    <div class="absolute top-10 right-10 md:top-0 md:right-0 md:relative opacity-40 hover:opacity-100">
-                        <button class="block" aria-label="Toggle dark mode" on:click={toggleDarkMode}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="24" height="24" fill="currentColor" aria-label="Moon Icon" class="w-6 h-6"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
-                        </button>
-                    </div>
-                    <a class="text-lg xl:text-xl font-bold rounded-full bg-dark dark:bg-light hover:opacity-80 px-[1em] py-[0.5em] text-light dark:text-dark" href="https://x.com/intent/follow?screen_name=loopsvariables" target="_blank">Follow</a>
+                    <a class="text-lg xl:text-xl font-bold rounded-full bg-dark dark:bg-light hover:opacity-80 px-[1em] py-[0.5em] text-light dark:text-dark" href="https://x.com/intent/follow?screen_name=okbudda" target="_blank">Follow</a>
                 </div>
             </div>
 
@@ -83,7 +78,7 @@
     </div>
 </div>
 
-<style>
+<style lang="postcss">
     /* Add any custom styles here if needed */
     :global(html) {
         @apply antialiased;
